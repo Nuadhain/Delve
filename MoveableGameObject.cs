9976718d6ddd92e95d5beedca8c977a2b5a3c768
BenLevinson <bml7725@g.rit.edu>
2015-03-30 18:01:30 -0400

    Create MoveableGameObject.cs

---
+++ MoveableGameObject.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace DelveCodeB
+{
+    public abstract class MoveableGameObject : GameObject
+    {
+        // attributes
+        protected int direction;
+        public int Direction // get and set properties
+        {
+            get { return direction; }
+            set { direction = value; }
+        }
+
+        public MoveableGameObject(int x, int y, int width, int height): base(x, y, width, height)
+        {
+
+        }
+        public abstract void TakeHit(); // create basic takehit method
+    }
+}
