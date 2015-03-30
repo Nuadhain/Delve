b3c625cae6cd13fc7adbd00a358b7160c411d015
BenLevinson <bml7725@g.rit.edu>
2015-03-30 18:01:10 -0400

    Create MapReader.cs

---
+++ MapReader.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DelveCodeB
+{
+    class MapReader
+    {
+        // mapreader class - will stream different maps and read them into the game
+        // not sure how to do this
+
+    }
+}
