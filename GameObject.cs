2f36f1bf460522c13950990607e2addcdf389bdb
BenLevinson <bml7725@g.rit.edu>
2015-04-10 13:52:24 -0400

    Update GameObject.cs

--- GameObject.cs
+++ GameObject.cs
@@ -20,8 +20,33 @@ namespace DelveCodeB
             get { return rect; }
             set { rect = value; }
         }
+        public int x;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
 
-        public GameObject(int x, int y, int width, int height)
+        public int y;
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public int width;
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+        public int height;
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public GameObject()
         {
             rect = new Rectangle(x, y, width, height);
             
